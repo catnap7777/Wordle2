879fe1b71db25bb7cc2663f9f54fc89ef6411b43
km <35552602+catnap7777@users.noreply.github.com>
2022-02-22 19:03:25 -0600

    Initial Commit

---
+++ Wordle2/Wordle2App.swift
@@ -0,0 +1,17 @@
+//
+//  Wordle2App.swift
+//  Wordle2
+//
+//  Created by Karen Mathes on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Wordle2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
